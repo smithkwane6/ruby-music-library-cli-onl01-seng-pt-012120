515f06aff833f5b4b8652b0670418fab6e815c56
Kwane Smith <smithkwane6@gmail.com>
2020-03-01 01:22:19 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,36 +1,36 @@
 class Genre
   extend Concerns::Findable
 
-  attr_accessor :name, :songs 
+  attr_accessor :name, :songs
 
   @@all = []
 
   def initialize(name)
     @name = name
     @songs = []
-  end 
+  end
 
 
   def self.all
     @@all
-  end 
+  end
 
-  def save 
-    @@all << self 
-  end 
+  def save
+    @@all << self
+  end
 
   def self.destroy_all
     @@all.clear
-  end 
+  end
 
   def self.create(name)
     new_genre = Genre.new(name)
     new_genre.save
     new_genre
-  end 
+  end
 
   def artists
-    @songs.collect {|song| song.artist}.uniq 
-  end 
+    @songs.collect {|song| song.artist}.uniq
+  end
 
 end  
