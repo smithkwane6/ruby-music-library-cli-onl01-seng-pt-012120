bc06b29f004aca3167554265a84b50441f6c40a1
Kwane Smith <smithkwane6@gmail.com>
2020-03-01 01:27:16 +0000

    Automatically backed up by Learn

--- lib/music_importer.rb
+++ lib/music_importer.rb
@@ -1,17 +1,17 @@
 class MusicImporter
-  attr_accessor :path 
+  attr_accessor :path
 
   def initialize(path)
     @path = path
-  end 
+  end
 
   def files
     @files = Dir.entries(@path)
     @files.delete_if {|file| file == "." || file == ".."}
-  end 
+  end
 
   def import
     files.each {|file| Song.create_from_filename(file)}
     #why not @files?
-  end 
+  end
 end  
